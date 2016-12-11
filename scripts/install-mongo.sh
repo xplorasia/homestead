85df12623f7bf1e0c41d44ae32f7da59c2a9680b
Ary Wibowo <nucreativa@gmail.com>
2016-12-11 09:51:26 +0700

    Configuration for xplor.asia

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Check If Maria Has Been Installed
 
 if [ -f /home/vagrant/.mongo ]
@@ -12,19 +13,19 @@ echo "MongoDB install  script with PHP7 & nginx [Laravel Homestead]"
 echo "By Zakaria BenBakkar, @zakhttp, zakhttp@gmail.com"
 
 echo "Importing the public key used by the package management system";
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927;
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 
 echo "Creating a list file for MongoDB.";
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list;
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
 echo "Updating the packages list";
-apt-get update;
+sudo apt-get update;
 
 echo "Install the latest version of MongoDb";
-apt-get install -y mongodb-org;
+sudo apt-get install -y mongodb-org;
 
 echo "Fixing the pecl errors list";
-sed -i -e 's/-C -n -q/-C -q/g' `which pecl`;
+sudo sed -i -e 's/-C -n -q/-C -q/g' `which pecl`;
 
 echo "Installing OpenSSl Libraries";
 apt-get install -y autoconf g++ make openssl libssl-dev libcurl4-openssl-dev;
@@ -32,27 +33,43 @@ apt-get install -y libcurl4-openssl-dev pkg-config;
 apt-get install -y libsasl2-dev;
 
 echo "Installing PHP7 mongoDb extension";
-pecl install mongodb;
-service mongod start;
+sudo pecl install mongodb;
 
 echo "adding the extension to your php.ini file";
-echo 'extension=mongodb.so' > /etc/php/mods-available/mongodb.ini;
-ln -s /etc/php/mods-available/mongodb.ini /etc/php/7.0/cli/conf.d/mongodb.ini;
-ln -s /etc/php/mods-available/mongodb.ini /etc/php/7.0/fpm/conf.d/mongodb.ini;
+sudo echo  "extension = mongodb.so" >> /etc/php/7.1/cli/php.ini;
+sudo echo  "extension = mongodb.so" >> /etc/php/7.1/fpm/php.ini;
 
-echo "restarting ngninx, php-fpm";
-service nginx restart && service php7.0-fpm restart;
+echo "Add mongodb.service file"
+cat >/etc/systemd/system/mongodb.service <<EOL
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+[Service]
+User=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl start mongodb
+sudo systemctl status mongodb
+sudo systemctl enable mongodb
+
+echo "restarting nginx, php-fpm";
+sudo service nginx restart && sudo service php7.1-fpm restart
 
 echo "adding user 'homestead' to admin collection";
-echo 'use admin;
+cat >/home/vagrant/setupMongo.js <<EOL
+use admin;
 db.createUser({
 	user: "homestead" ,
 	pwd: "secret",
 	roles: [
 		"root"
 	]
-});' > /home/vagrant/apps/setupMongo.js
-mongo admin < /home/vagrant/apps/setupMongo.js
+});
+EOL
+mongo admin < /home/vagrant/setupMongo.js
 
 echo "configuring /etc/mongod.conf";
 sudo sed -i "s/  bindIp: 127.0.0.1/# bindIp: 127.0.0.1/g" /etc/mongod.conf
@@ -61,4 +78,4 @@ a="\ \ authorization: enabled"
 sudo sed -i "/security:/a ${a}" /etc/mongod.conf
 
 echo "restarting mongodb after enabling security";
-service mongod restart;
+sudo service mongodb restart;
